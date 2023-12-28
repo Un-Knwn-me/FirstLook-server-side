1a7fc14dab1f1dfd8a206fa8f6ecc4fb7dc10fb7
Un-Knwn-me <raghul9510@gmail.com>
2023-12-29 02:16:55 +0530

    commit 6.34

--- middlewares/storage.js
+++ middlewares/storage.js
@@ -4,9 +4,11 @@ const multerGoogleStorage = require('multer-google-storage');
 const multer = require('multer');
 require('dotenv').config();
 
+const gcredentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
 const storage = new Storage({
   // keyFilename: path.join(__dirname, '../mad-monkeyz-c2e97e57f277.json'),
-  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+  keyFilename: gcredentials,
   projectId: process.env.ProjectId,
 });
 
@@ -16,7 +18,7 @@ const multerStorage = multerGoogleStorage.storageEngine({
   autoRetry: true,
   bucket: 'firstlook-ecommerce',
   projectId: 'mad-monkeyz',
-  credentials: JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS),
+  keyFilename: gcredentials,
   // keyFilename: path.join(__dirname, '../mad-monkeyz-c2e97e57f277.json'),
   filename: (req, file, cb) => {
     cb(null, file.originalname);    

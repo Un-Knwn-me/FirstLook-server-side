607c4a9ada12f902832e4fad5b9ed7254f4d897b
Un-Knwn-me <raghul9510@gmail.com>
2023-12-30 11:49:14 +0530

    commit 6.34

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,5 +1,10 @@
 let fs = require('fs');
 
 fs.writeFile(process.env.GOOGLE_APPLICATION_CREDENTIALS, process.env.GOOGLE_CREDENTIALS, (err) => {
-    throw err;
+    if (err) {
+        console.error('Error writing Google Cloud credentials:', err);
+        process.exit(1);
+    } else {
+        console.log('Google Cloud credentials written successfully');
+    }
 });
